c6b5c650910b0df4ddcee6451cb008ed27ffa844
Thales Ruano <thalesoruano@gmail.com>
2022-02-14 16:47:54 -0300

    fix url of base url

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Base_URL = "https://back-library.herokuapp.com/";
+const Base_URL = "https://back-library.herokuapp.com";
 
 function createConfig(token) {
 	return {
